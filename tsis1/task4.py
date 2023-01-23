2de856232157693ebfede8d84a649ba99b3b4926
Yermakhan  Kassymzhomart <123319945+pp2-22B030349@users.noreply.github.com>
2023-01-23 20:24:06 +0600

    Create task4.py

---
+++ tsis1/task4.py
@@ -0,0 +1,19 @@
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+print("Hello, World!") #This is a comment
+
+#This is a comment
+print("Hello, World!")
