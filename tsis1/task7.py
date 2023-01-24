bab68a02e45a93905aa58afbaf2d8b9889233dc1
Yermakhan  Kassymzhomart <123319945+pp2-22B030349@users.noreply.github.com>
2023-01-24 18:19:50 +0600

    Create task7.py

---
+++ tsis1/task7.py
@@ -0,0 +1,16 @@
+x = 12
+y = 0.1
+z = 6j
+print(type(x), type(y), type(z))
+a = complex(x)
+print(a)
+a = complex(y)
+print(a)
+a = complex(z)
+print(a)
+
+b = int(x)
+print(b)
+b = int(y)
+print(b)
+# b = int(z) -> error
