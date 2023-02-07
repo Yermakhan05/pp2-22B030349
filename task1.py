e049f99c3e34ee48d6cc743f6f12a09898c95973
Kasymzhomart Ermakhan <myandasik05@gmail.com>
2023-02-07 19:52:09 +0600

    tsis3, task1

--- task1.py
+++ task1.py
@@ -1,1 +1,10 @@
-print("hello, world")
+class word:
+    def getString(self):
+        self.s = input()
+
+    def printString(self):
+        print(self.s.upper())
+
+ans = word()
+ans.getString()
+ans.printString()
